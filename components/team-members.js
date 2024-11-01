4a32f5f6ff97237fa8f6368aafd75d4f4f9ed4f4
herosolutions94 <herosolutions.tk@gmail.com>
2024-11-01 14:41:01 +0500

    yy

--- components/team-members.js
+++ components/team-members.js
@@ -57,6 +57,7 @@ export default function TeamMembers({teamMembers, content}) {
                 </div>
               </div>
                       ))}
+                      
 
               
             </div>
